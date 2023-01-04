2335214522b60ff435122e7a23a84f61f0773188
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-01-04 12:01:47 -0300

    Added and Fixing color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,7 +13,8 @@ module.exports = {
       '2xl': 32
     },
     colors:{
-      'black': '#000000',
+      black: '#000000',
+      white: '#FFFFFF',
 
       'gray-900': '#121214',
       'gray-800': '#202024',
@@ -22,7 +23,7 @@ module.exports = {
       'gray-100': '#E1E1E6',
 
       'cyan-500': '#81D8F7',
-      'cyan-500': '#9BE1FB',
+      'cyan-300': '#9BE1FB',
     },
     extend: {
       fontFamily: {
