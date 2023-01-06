a707387df24bde68e151b8036f1428f396134b98
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-01-05 22:43:23 -0300

    Create a shortcut of components

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+import { Button } from './Button'
+import { Checkbox } from './Checkbox'
+import { Heading } from './Heading'
+import { Text } from './Text'
+import { TextInput } from './TextInput'
+
+export {
+    Button,
+    Checkbox,
+    Heading,
+    Text,
+    TextInput,
+}
